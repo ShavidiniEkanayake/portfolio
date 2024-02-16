551c08022c2ac19105ddc7ef0856f7a97cb0300c
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2024-02-16 18:25:01 +0530

    Fixed issues in home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,20 +5,19 @@ export const Home = () => {
   return (
     <div>
       <div
-        className={`bg-bgorange dark:bg-darkmode text-black h-svh flex items-center justify-center relative`}
+        className={`bg-bgorange dark:bg-darkmode text-black h-[calc(100vh-70px-46px)]  xs:h-[calc(100vh-90px-46px)] flex items-center justify-center relative `}
       >
         <div className="relative animate-fade-in">
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div
-              style={{ width: "38vw", height: "38vw" }}
-              className="bg-black dark:bg-gray-800 rounded-full flex items-center justify-center"
+              className="bg-black dark:bg-white rounded-full flex items-center justify-center w-[90vw] h-[90vw] sm:w-[70vw] sm:h-[70vw] xsm:w-[80vw] xsm:h-[80vw] md:w-[54vw] md:h-[54vw] lg:w-[37vw] lg:h-[37vw] py-10"
             ></div>
           </div>
           <div className="text-center">
-            <h1 className="text-[5vw] font-Satoshi font-black bg-bgorange p-2 sm:p-4 md:pt-6 lg:pt-8 lg:pb-4 md:pb-3 sm:pb-1">
+            <h1 className="text-[8vw] md:text-[7vw] lg:text-[5vw] px-10 font-Satoshi font-black bg-bgorange py-2 sm:py-4 md:pt-6 lg:pt-8 lg:pb-4 md:pb-3 sm:pb-1">
               Creative Developer
             </h1>
-            <h1 className="text-[7vw] font-Satoshi font-normal bg-bgorange p-2 sm:p-4 md:pb-6 lg:pb-8 uppercase">
+            <h1 className="text-[9vw] md:text-[8vw] lg:text-[6svw] px-20 font-Satoshi font-normal bg-bgorange py-2 sm:py-4 md:pb-6 lg:pb-8 uppercase">
               Based in Sri Lanka
             </h1>
           </div>
