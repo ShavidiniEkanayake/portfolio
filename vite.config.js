311e07f6c3f03e763fc93eac36713bb1d5e1bbeb
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2024-03-12 11:14:27 +0530

    Changed base path in vite confige file

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, './src')}/`,
     },
   },
-  
 });
 
 
